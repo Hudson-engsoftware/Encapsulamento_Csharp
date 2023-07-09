b57d2a1f905424f993486496f1c085a4a5151e1a
hudson <hudsonengesoftware@outlook.com>
2023-07-09 17:29:14 -0300

    Encapsulamento

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Globalization;
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            Produto produto = new Produto("TV",500.00,10);
+            Console.WriteLine(produto.GetNome());
+
+
+        }
+    }
+}
